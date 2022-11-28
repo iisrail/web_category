450ca3d9cd1fc0ab397c4a1da6cd45aae54d3cb4
igal israilevich <iisrail@gmail.com>
2022-11-28 02:23:51 +0200

    Update RunnerTest.java

--- src/test/java/com/webscrap/assg/api/service/impl/RunnerTest.java
+++ src/test/java/com/webscrap/assg/api/service/impl/RunnerTest.java
@@ -1,18 +1,21 @@
 package com.webscrap.assg.api.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.webscrap.assg.api.model.Category;
-
 @SpringBootTest
 public class RunnerTest {
 	@Autowired
 	Runner service;
+
 	@Test
 	public void testClassify() {
 		List<String> pages = new ArrayList<>();
@@ -22,7 +25,11 @@ public class RunnerTest {
 		pages.add("https://edition.cnn.com/sport");
 		names.add("Star Wars");
 		names.add("Basketball");
-		service.classify(pages, names);
+		Map<String, List<String>> map = service.classify(pages, names);
+		assertFalse(map.get("http://www.starwars.com").contains("Basketball"));
+		assertTrue(map.get("https://www.imdb.com/find?q=star+wars&ref_=nv_sr_sm").contains("Star Wars"));
+		assertTrue(map.get("https://edition.cnn.com/sport").contains("Basketball"));
+
 	}
 
 }
